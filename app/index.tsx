b67f09a175936b251a3fdbb8dcd5537428a8f5bb
nbsmile-projects <beksultanschool@gmail.com>
2025-02-08 00:30:56 +0600

    created navbuttons on the mainPage

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,38 @@
-import { Text, View } from "react-native";
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    gap: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    width: 200,
+    height: 50,
+    borderWidth: 1,
+    borderColor: "black",
+  },
+  buttonText: {
+    paddingVertical: "5%",
+    textAlign: "center",
+    color: "black",
+    fontSize: 16,
+  },
+});
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.view}>
+      <TouchableOpacity style={styles.button} onPress={() => alert("Pressed!")}>
+        <Text style={styles.buttonText}>Создать</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => alert("Pressed!")}>
+        <Text style={styles.buttonText}>История</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => alert("Pressed!")}>
+        <Text style={styles.buttonText}>Выйти</Text>
+      </TouchableOpacity>
     </View>
   );
 }
